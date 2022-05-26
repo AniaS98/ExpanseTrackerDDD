6db4366a8ef3f234e906a66c66d345f85727c885
Ania <Ania@DESKTOP-E08E0TL>
2022-05-26 22:00:54 +0200

    Dodaj pliki projektów.

---
+++ ExpanseTrackerDDD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExpanseTrackerDDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
